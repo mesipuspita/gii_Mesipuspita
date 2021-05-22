65605379ce272dd87662c1b652d1d321a38ec448
DESKTOP-I2JGHSI\HP <mhesi202@gmail.com>
2021-05-22 10:27:59 +0700

    GII_CRUD _Mesi puspita sari

---
+++ models/Jenis.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "jenis".
+ *
+ * @property int $id
+ * @property string $nama
+ * @property string $keterangan
+ */
+class Jenis extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'jenis';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nama', 'keterangan'], 'required'],
+            [['nama', 'keterangan'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nama' => 'Nama',
+            'keterangan' => 'Keterangan',
+        ];
+    }
+}
